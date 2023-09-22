46366122c529bb710ce04c3a88624a2ed6b7fe6f
Axel <20b3010455@untels.edu.pe>
2023-09-22 03:17:32 -0500

    app.py

--- app.py
+++ app.py
@@ -1,47 +1,49 @@
-# Función para convertir una palabra a su representación ASCII
+# funcion conversion de palabra a ascii
 def palabra_a_ascii(palabra):
-    ascii_values = [str(ord(caracter)) for caracter in palabra]
-    return "".join(ascii_values)
+    valor_ascii = [str(ord(caracter)) for caracter in palabra]
+    return "".join(valor_ascii)
 
-# Función para convertir un número entero a representación binaria
+# funcion conversion de numero a binario
 def decimal_a_binario(decimal):
     if decimal <= 0:
         return "0"
-    # Aquí almacenamos el resultado
     binario = ""
-    # Mientras se pueda dividir...
     while decimal > 0:
-        # Saber si es 1 o 0
         residuo = int(decimal % 2)
-        # E ir dividiendo el decimal
         decimal = int(decimal / 2)
-        # Ir agregando el número (1 o 0) a la izquierda del resultado
         binario = str(residuo) + binario
     return binario
 
-# Inicializar las variables fuera de los bucles
+# Variables inicializadas en 0
 TOTALCPU = 0
 TOTALCiclos = 0
 
-# Leer la canción desde un archivo de texto
+# leer cancion de un bloc de notas de nuestra carpeta
 with open("cancion.txt", "r") as archivo:
     canciones = archivo.readlines()
 
-# Medir el tiempo de CPU y los ciclos de reloj
+
 for cancion in canciones:
     palabras = cancion.split()
     for palabra in palabras:
+        # Tiempo CPUS
         TCPU = len(palabra) * 2
+        #CICLOS RELOJ
         CRELOJ = 4 * TCPU
+        # SUMATORIA DE TODOS LOS TIEMPOS CPU
         TOTALCPU += TCPU
+        # SUMATORIA DE TODOS LOS CICLOS RELOJ
         TOTALCiclos += CRELOJ
-
-        ascii_values = palabra_a_ascii(palabra)
-        binario_values = decimal_a_binario(int(ascii_values))
+        
+        # CONVERSION PALABRA A ASCII
+        valor_ascii = palabra_a_ascii(palabra)
+        
+        # CONVERSION DECIMAL A BINARIO
+        binario = decimal_a_binario(int(valor_ascii))
 
         print(f"Unidad de analisis: {palabra}")
-        print(f"ASCII: {ascii_values}")
-        print(f"BINARIO: {binario_values}")
+        print(f"ASCII: {valor_ascii}")
+        print(f"BINARIO: {binario}")
         print(f"Tiempo CPUS: {TCPU}")
         print(f"CICLOS RELOJ: {CRELOJ}")
         print("*" * 50)
